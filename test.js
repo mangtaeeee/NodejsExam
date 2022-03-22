2d9fb8fe9612ec7879cc3af21d90ff77bf7a8832
kimtaeyoon <90013740+mangtaeeee@users.noreply.github.com>
2022-03-22 15:42:48 +0900

    Update test.js

--- test.js
+++ test.js
@@ -4,8 +4,8 @@ const email = {
     host: "smtp.mailtrap.io",
     port: 2525,
     auth: {
-      user: "c67ac0beb7ca46",
-      pass: "6121db4098ef29"
+      user: "",
+      pass: ""
     }
 };
 const send = async(option) => {
@@ -20,8 +20,8 @@ const send = async(option) => {
 };
 
 let email_data = {
-    from: "osp9658@gmail.com",
-    to: "osp9658@gmail.com",
+    from: "osp9***@gmail.com",
+    to: "osp9***@gmail.com",
     subject : "처음 보내는 메일입니다.",
     text : "nodejs 이메일 보내기"
 }
